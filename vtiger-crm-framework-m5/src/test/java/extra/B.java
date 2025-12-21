9f2eb1312660a1ccc927dd5edbe797b8826d87f3
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2025-12-21 22:19:21 +0530

    new

--- vtiger-crm-framework-m5/src/test/java/extra/B.java
+++ vtiger-crm-framework-m5/src/test/java/extra/B.java
@@ -15,7 +15,22 @@ class A {
 
 public class B {
 	public static void main(String[] args) {
-		A a = new A("shashank");
-		a.printLocal();
+//		A a = new A("shashank");
+//		a.printLocal();
+
+		String nick = "Mahi";
+
+		String fn = "Mahendra('" + nick + "')";
+		String mn = "singh";
+		String ln = "Dhoni";
+
+		String fullName = fn + " " + mn + " " + ln;
+		System.out.println(fullName);
+
+//		dynamic xpath
+		String orgName = "phonepe";
+		String xpath = "//a[text()='" + orgName + "']";
+		System.out.println(xpath);
+
 	}
 }
